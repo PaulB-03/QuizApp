d4a020f429adbf958f282d9cb903199ca1bb9e5f
Paul Burmester <paulburmester2@gmail.com>
2025-07-09 16:50:09 +0200

    added main js code to handel questions ans answers

--- script.js
+++ script.js
@@ -38,5 +38,101 @@ let questions = [
         "answer_3": "<li>",
         "answer_4": "<list>",
         "right_answer": 2
+    },
+    {
+        "question": "Wie wird ein Link in HTML definiert?",
+        "answer_1": "<link>",
+        "answer_2": "<a>",
+        "answer_3": "<href>",
+        "answer_4": "<url>",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Attribut wird verwendet, um eine CSS-Klasse in HTML zu definieren?",
+        "answer_1": "class",
+        "answer_2": "id",
+        "answer_3": "style",
+        "answer_4": "type",
+        "right_answer": 1
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um eine Tabelle zu erstellen?",
+        "answer_1": "<table>",
+        "answer_2": "<tr>",
+        "answer_3": "<td>",
+        "answer_4": "<th>",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie bindet man ein Bild in HTML ein?",
+        "answer_1": "<image src='url'>",
+        "answer_2": "<img src='url'>",
+        "answer_3": "<picture src='url'>",
+        "answer_4": "<figure src='url'>",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Attribut im <script>-Tag sorgt dafür, dass das Skript erst nach dem vollständigen Parsen des Dokuments ausgeführt wird?",
+        "answer_1": "async",
+        "answer_2": "nomodule",
+        "answer_3": "defer",
+        "answer_4": "type",
+        "right_answer": 3
     }
 ];
+
+let currentQuestionIndex = 0;
+
+function init() {
+    document.getElementById("total-questions").innerText = questions.length;
+    showQuestion();
+
+
+}
+
+function showQuestion() {
+
+    if (currentQuestionIndex >= questions.length) {
+        // TODO: Show end screen with results
+        document.getElementById("endScreen").style.display = "block";
+        document.getElementById("questionBody").style.display = "none";
+        document.getElementById("quiz-logo").style.display = "none";
+    }
+
+    let question = questions[currentQuestionIndex];
+    document.getElementById("question-text").innerText = question.question;
+    document.getElementById("answer_1").innerText = question.answer_1;
+    document.getElementById("answer_2").innerText = question.answer_2;
+    document.getElementById("answer_3").innerText = question.answer_3;
+    document.getElementById("answer_4").innerText = question.answer_4;
+}
+
+function selectAnswer(answerIndex) {
+    let question = questions[currentQuestionIndex];
+    let selectedAnswer = answerIndex.slice(-1);
+    let idOfRightAnswer = "answer_" + question.right_answer;
+
+    if(selectedAnswer == question["right_answer"]) {
+        document.getElementById(answerIndex).parentNode.classList.add("bg-success");
+    } else {
+        document.getElementById(answerIndex).parentNode.classList.add("bg-danger");
+        document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    }
+    document.getElementById("next_button").disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestionIndex++;
+
+    document.getElementById("next_button").disabled = true;
+    document.querySelectorAll(".quiz-answer-card").forEach(card => {
+        card.classList.remove("bg-success", "bg-danger");
+    });
+
+    showQuestion();
+    updateCurrentQuestion();
+}
+
+function updateCurrentQuestion() {
+    document.getElementById("current-question").innerText = currentQuestionIndex + 1;
+}
