4cba89376e0b2829fb5e174475bc619b9208aae3
Paul Burmester <paulburmester2@gmail.com>
2025-07-09 15:22:46 +0200

    added the 5 easy questions about html

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Rasmus Lerdorf",
+        "answer_2": "Brendan Eich",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "David Heinemeier Hansson",
+        "right_answer": 3
+    },
+    {
+        "question": "Wofür steht die Abkürzung HTML?",
+        "answer_1": "Hypertext Markup Level",
+        "answer_2": "Hypertext Markup Language",
+        "answer_3": "Hyperlink and Text Markup Language",
+        "answer_4": "Hypertext Markup Link",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Tag wird in HTML benutzt, um einen Absatz zu definieren?",
+        "answer_1": "<p>",
+        "answer_2": "<div>",
+        "answer_3": "<span>",
+        "answer_4": "<section>",
+        "right_answer": 1
+    },
+    {
+        "question": "Welches Attribut wird verwendet, um eine Bildquelle in HTML zu definieren?",
+        "answer_1": "title",
+        "answer_2": "href",
+        "answer_3": "alt",
+        "answer_4": "src",
+        "right_answer": 4
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um eine ungeordnete Liste zu erstellen?",
+        "answer_1": "<ol>",
+        "answer_2": "<ul>",
+        "answer_3": "<li>",
+        "answer_4": "<list>",
+        "right_answer": 2
+    }
+];
